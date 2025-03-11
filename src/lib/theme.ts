5c3e731be305f71948244f2309271558b03cb447
amon.felbermayer <amon.felbermayer@nesto-software.de>
2025-03-11 07:27:12 +0100

    Define menu item colors in theme file

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -21,4 +21,17 @@ const config = defineConfig({
   },
 });
 
+export const menuItemColors = {
+  bgCardLight: "orange.200",
+  bgCardDark: "orange.600",
+  bgImageLight: "orange.200",
+  bgImageDark: "orange.600",
+  bgButtonLight: "orange.600",
+  bgButtonDark: "orange.200",
+  colorDescriptionLight: "orange.600",
+  colorDescriptionDark: "orange.200",
+  colorButtonLight: "white",
+  colorButtonDark: "black",
+};
+
 export const system = createSystem(defaultConfig, config);
