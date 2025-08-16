149d6c06600952ed139cf7a574637446b6926fff
afkdev8 <mr.infinidy@protonmail.com>
2025-08-17 00:56:29 +0200

    Add water to menu

--- src/app/drinks/page.tsx
+++ src/app/drinks/page.tsx
@@ -4,9 +4,10 @@ import TitleLayout from "@/lib/titleLayout";
 import MenuItem from "@/components/menuItem";
 import beer from "@/menuData/drinks/beer";
 import redWine from "@/menuData/drinks/redwine";
+import water from "@/menuData/drinks/water";
 
 const Food = () => {
- return (
+  return (
     <Container>
       <MenuLayout>
         <Stack gap="4" direction="column">
@@ -16,6 +17,7 @@ const Food = () => {
           <Stack gap="4" direction="row" wrap="wrap">
             <MenuItem name={beer.name} description={beer.description} price={beer.price} imageSrc={beer.imageSrc} />
             <MenuItem name={redWine.name} description={redWine.description} price={redWine.price} imageSrc={redWine.imageSrc} />
+            <MenuItem name={water.name} description={water.description} price={water.price} imageSrc={water.imageSrc} />
           </Stack>
         </Stack>
       </MenuLayout>
---
+++ src/menuData/drinks/water.ts
@@ -0,0 +1,11 @@
+
+import { MenuItemProps } from "@/components/menuItem";
+
+const water: MenuItemProps = {
+  name: 'water',
+  description: 'Fresh water from a local spring',
+  price: 2.9,
+  imageSrc: 'images/drinks/water.png',
+}
+
+export default water;
