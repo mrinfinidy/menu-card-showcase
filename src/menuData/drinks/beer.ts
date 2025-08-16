f669843a34b72140220e40e2e844dd593c3ad1d2
afkdev8 <mr.infinidy@protonmail.com>
2025-08-17 00:40:56 +0200

    Price Change

--- src/menuData/drinks/beer.ts
+++ src/menuData/drinks/beer.ts
@@ -3,7 +3,7 @@ import { MenuItemProps } from "@/components/menuItem";
 const beer: MenuItemProps = {
   name: 'beer',
   description: 'A refreshing beer brewed in-house in traditional italian style',
-  price: 5.90,
+  price: 4.90,
   imageSrc: 'images/drinks/beer.png',
 }
 
--- src/menuData/food/pizza.ts
+++ src/menuData/food/pizza.ts
@@ -3,7 +3,7 @@ import { MenuItemProps } from "@/components/menuItem"
 const pizza: MenuItemProps = {
   name: 'Pizza',
   description: 'A delicious pizza with tomato sauce and mozzarella',
-  price: 10.90,
+  price: 8.9,
   imageSrc: 'images/food/pizza.png',
 }
 
