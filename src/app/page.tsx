4b01b231fc95820e7cfc85e40aa2b0b1a293b018
afkdev8 <mr.infinidy@protonmail.com>
2025-08-17 13:20:20 +0200

    Replace blue front home image with sperlonga

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import TextLayout from "@/lib/textLayout";
 
 const Home = () => {
-  const blueFrontImg = "/images/restaurant/blue-front.jpg";
+  const sperlongaImg = "/images/restaurant/sperlonga.jpg";
   const siciliaImg = "/images/restaurant/sicilia.jpg";
 
   return (
@@ -15,9 +15,9 @@ const Home = () => {
           Welcome to my restaurant
         </TitleLayout>
         <Stack gap="4" direction="column">
-          <Image src={blueFrontImg} alt="Blue Front" width={1920} height={1271} style={{ borderRadius: 8 }} />
+          <Image src={sperlongaImg} alt="Sperlonga" width={1920} height={1271} style={{ borderRadius: 8 }} />
           <TextLayout>
-            Enjoy delicious food with a stunning view of the Mediterranean Sea.
+            Enjoy delicious Italian food with a stunning view of the Mediterranean Sea.
           </TextLayout>
           <Image src={siciliaImg} alt="Sicilia" width={1920} height={1271} style={{ borderRadius: 8 }} />
         </Stack>
