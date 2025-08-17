b80df1b068e707580a1efb1347eddd73b408b360
afkdev8 <mr.infinidy@protonmail.com>
2025-08-17 03:18:49 +0200

    Add photo of sicily to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import { Container, Stack } from "@chakra-ui/react";
 import MenuLayout from "@/lib/menuLayout";
 import TitleLayout from "@/lib/titleLayout";
 import Image from "next/image";
+import TextLayout from "@/lib/textLayout";
 
 const Home = () => {
   const blueFrontImg = "/images/restaurant/blue-front.jpg";
+  const siciliaImg = "/images/restaurant/sicilia.jpg";
 
   return (
     <Container>
@@ -14,6 +16,10 @@ const Home = () => {
         </TitleLayout>
         <Stack gap="4" direction="column">
           <Image src={blueFrontImg} alt="Blue Front" width={1920} height={1271} style={{ borderRadius: 8 }} />
+          <TextLayout>
+            Enjoy delicious food with a stunning view of the Mediterranean Sea.
+          </TextLayout>
+          <Image src={siciliaImg} alt="Sicilia" width={1920} height={1271} style={{ borderRadius: 8 }} />
         </Stack>
       </MenuLayout>
     </Container>
